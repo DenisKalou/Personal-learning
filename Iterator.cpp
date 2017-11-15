28f95a1a55f90335125de62e9f4353daf52d7102
DenisKalou <32569581+DenisKalou@users.noreply.github.com>
2017-11-15 17:52:28 +0300

    Update Iterator.cpp

--- Iterator.cpp
+++ Iterator.cpp
@@ -1,12 +1,10 @@
-// try_it.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
+#include <cassert>
 #include <iterator>
 #include <iostream>
 #include <list>
 #include <vector>
-using namespace std;
+//using namespace std;
 
 
 template<class T>
@@ -41,7 +39,7 @@ public:
 	// определите метод size
 	size_t size() const
 	{
-		size_t size = 0;
+		std::size_t size = 0;
 		for (auto& a : data_) {
 			for (auto& b : a) {
 				size++;
@@ -51,63 +49,66 @@ public:
 	}
 
 	// определите const_iterator
-	class const_iterator 
-		: public std::iterator<forward_iterator_tag, VectorList> 
+	class const_iterator
+		: public std::iterator<std::bidirectional_iterator_tag, const T, std::ptrdiff_t,  const T*, const T&>
 	{
 	public:
-		const_iterator(const T* p)
-			: it_data_(p) {}
-		const_iterator(const const_iterator& it) 
-			: it_data_(it.it_data_){}
+		const_iterator(const T* i = nullptr) : ptr(i) {}
+		const_iterator(const const_iterator &other) : ptr(other.ptr) {}
+		const_iterator& operator=(const const_iterator &other) = default;
+		const_iterator& operator=(const T* i) {	ptr = i; return *this;	}
 		~const_iterator() {}
-		T operator*() const { return *it_data_; }
-		bool operator==(const const_iterator& it) { return it_data_ == it.it_data_; }
-		bool operator!=(const const_iterator& it) {	return !(it_data_ == it.it_data_); }
-		const T* operator++() { return ++it_data_; }
-		const T* operator++(int) {	return it_data_++; }
-	private:
-		const T* it_data_;
+		const T& operator*() { return *ptr; }
+		const T& operator*() const { return *ptr;}
+		const T* operator->() { return ptr;}
+		const_iterator& operator++() { ++ptr; return *this;	}
+		const_iterator operator++(int) {auto tmp(*this);	++ptr;	return tmp;	}
+		const_iterator& operator--() { --ptr; return *this;	}
+		const_iterator operator--(int) { auto tmp(*this);	--ptr;	return tmp;	}
+		bool operator==(const const_iterator &other) const { return (ptr == other.ptr);	}
+		bool operator!=(const const_iterator &other) const { return !(*this == other); }
+	protected:
+		const T* ptr;
 	};
-	
-		// определите методы begin / end
-	const_iterator begin() const { 
-		const_iterator it(&*(data_.begin())->begin());
-		return it;
+	// определите методы begin / end
+	const_iterator begin() const {
+		return const_iterator(&(data_.front()[0]));
 	}
-	const_iterator end()   const { 
-		const_iterator it(&*(data_.begin())->begin() + this->size());
-		return it;
+	const_iterator end()  const {
+		return const_iterator (&(data_.front()[0]) + this->size());
 	}
-	
 	// определите const_reverse_iterator
-	struct const_reverse_iterator 
-		: std::iterator<forward_iterator_tag, VectorList> 
+	class const_reverse_iterator
+		: const_iterator
 	{
 	public:
-		const_reverse_iterator(const T* p)
-			: it_data_(p) {}
-		const_reverse_iterator(const const_reverse_iterator& it)
-			: it_data_(it.it_data_) {}
+		const_reverse_iterator(const T* i = nullptr) : const_iterator(i) {}
+		const_reverse_iterator(const const_iterator &other) { this->ptr = other.ptr; }
+		const_reverse_iterator& operator=(const const_reverse_iterator &other) = default;
+		const_reverse_iterator& operator=(const T* i) { this->ptr = i; return *this; }
 		~const_reverse_iterator() {}
-		T operator*() const { return *it_data_; }
-		bool operator==(const const_reverse_iterator& it) { return it_data_ == it.it_data_; }
-		bool operator!=(const const_reverse_iterator& it) { return !(it_data_ == it.it_data_); }
-		const_reverse_iterator& operator++() { return --it_data_; }
-		const_reverse_iterator& operator++(int) { return it_data_--; }
-	private:
-		const T* it_data_;
+		const T& operator*() { return *this->ptr; }
+		const T& operator*() const { return *this->ptr; }
+		const T* operator->() { return this->ptr; }
+		const_iterator& operator++() { ++this->ptr; return *this; }
+		const_iterator operator++(int) { auto tmp(*this);	++this->ptr;	return tmp; }
+		const_iterator& operator--() { --this->ptr; return *this; }
+		const_iterator operator--(int) { auto tmp(*this);	--this->ptr;	return tmp; }
+		bool operator==(const const_iterator &other) const { return (this->ptr == other.ptr); }
+		bool operator!=(const const_iterator &other) const { return !(*this == other); }
+
 	};
 
-		// определите методы rbegin / rend
+	// определите методы rbegin / rend
 	const_reverse_iterator rbegin() const {
-		const_reverse_iterator it(&*(data_.end())->end());
+		const_reverse_iterator it(&*(data_.begin())->begin() + this->size() - 1);
 		return it;
 	}
 	const_reverse_iterator rend()   const {
-		const_reverse_iterator it(&*(data_.begin())->begin());
+		const_reverse_iterator it(&*(data_.begin())->begin() - 1, size());
 		return it;
 	}
-
+	
 private:
 	ListT data_;
 };
@@ -121,6 +122,11 @@ void VectorList<T>::append(It p, It q)
 }
 
 int main() {
+
+	VectorList<int> v;
+	assert(std::distance(v.begin(), v.end()) == 0);
+	assert(std::distance(v.rbegin(), v.rend()) == 0);
+
 	VectorList<int> vlist;
 
 	std::vector<int> v1;
@@ -136,8 +142,16 @@ int main() {
 	vlist.append(v1.begin(), v1.end());
 	vlist.append(v2.begin(), v2.end());
 
-	//cout << vlist.size() << endl;
-	auto a = vlist.begin();
+	//std::vector<int>::iterator vit = v1.begin();
+	//auto it = vlist.end();
+	size_t size = vlist.size();
+	for (auto it = vlist.begin(); it != vlist.end(); ++it) {
+		std::cout << *it << " " << std::endl;
+	//std::cout << *(vlist.begin()) << " " << std::endl;
+	}
+
+	//cout << typeid(vit).name() << endl;
+	//auto a = vlist.begin();
 	//vlist.data();
 	return 0;
 };
