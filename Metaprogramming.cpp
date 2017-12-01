b7e5743bdf29e469c8759433722a29e91dd66540
DenisKalou <32569581+DenisKalou@users.noreply.github.com>
2017-12-01 15:38:40 +0300

    Update Metaprogramming.cpp

--- Metaprogramming.cpp
+++ Metaprogramming.cpp
@@ -68,7 +68,7 @@ struct Minus
 template<class L1, class L2, template<int...> class F>
 struct Zip;
 
-template<template<int...> class L1, int... Ints1, 
+template<template<int...> class L1, int... Ints1,
 	template<int...> class L2, int... Ints2,
 	template<int...> class F>
 struct Zip<L1<Ints1...>, L2<Ints2...>, F>
@@ -76,49 +76,78 @@ struct Zip<L1<Ints1...>, L2<Ints2...>, F>
 	using type = L1<F<Ints1, Ints2>::value...>;
 };
 
-template<class... L>
-struct Quantity;
-
-template<int... Ints>
-struct Quantity<IntList<Ints...>> {
-private:
-	
-	double val_;
+template<class T>
+struct Quantity {
 public:
-	using type = IntList<Ints...>;
-	explicit Quantity(const double& val)
-		: val_(val) {};
+	Quantity()
+		: val_{} {};
+	explicit Quantity(double val)
+		: val_{ val } {};
 	~Quantity() {};
-	double& value() const{
+	double value() const {
 		return val_;
 	}
-	double& operator/(const Quantity& other) {
-		val_ *= other.val_;
-		using type = typename Zip<type, typename other::type, Minus>::type;
+	Quantity operator+(const Quantity& other) const {
+		return Quantity<T>{val_ + other.val_};
 	}
+	Quantity operator-(const Quantity& other) const {
+		return Quantity<T>{val_ - other.val_};
+	}
+	Quantity operator*(const double val) const {
+		return Quantity<T>{val_ * val};
+	}
+	Quantity operator/(const double val) const {
+		return Quantity<T>{val_ / val};
+	}
+private:
+	double val_;
 };
+
+template<class T>
+Quantity<T> operator*(const double val, const Quantity<T>& q) {
+	return q * val;
+}
+
+
+template<class L1, class L2, class Q = Quantity<typename Zip<L1, L2, Plus>::type>>
+Q operator*(const Quantity<L1>& lq, const Quantity<L2>& rq) {
+	return Q{ lq.value() * rq.value()};
+}
+template<class L1, class L2, class Q = Quantity<typename Zip<L1, L2, Minus>::type>>
+Q operator/(const Quantity<L1>& lq, const Quantity<L2>& rq) {
+	return Q{ lq.value() / rq.value() };
+}
+template<class L, class Q = Quantity<typename Zip<Dimension<>, L, Minus>::type>>
+Q operator/(const double val, const Quantity<L>& q) {
+	return Q{ val / q.value() };
+}
+
 
 template<int m = 0, int kg = 0, int s = 0, int A = 0, int K = 0, int mol = 0, int cd = 0>
 using Dimension = IntList<m, kg, s, A, K, mol, cd>;
 
-using NumberQ = Quantity<Dimension<>>;          // число без размерности
-using LengthQ = Quantity<Dimension<1>>;          // метры
-using MassQ = Quantity<Dimension<0, 1>>;       // килограммы
-using TimeQ = Quantity<Dimension<0, 0, 1>>;    // секунды
-using VelocityQ = Quantity<Dimension<1, 0, -1>>;   // метры в секунду
-using AccelQ = Quantity<Dimension<1, 0, -2>>;   // ускорение, метры в секунду в квадрате
-using ForceQ = Quantity<Dimension<1, 1, -2>>;   // сила в ньютонах
+
 
 int main()
 {
+	using NumberQ = Quantity<Dimension<>>;          // число без размерности
+	using LengthQ = Quantity<Dimension<1>>;          // метры
+	using MassQ = Quantity<Dimension<0, 1>>;       // килограммы
+	using TimeQ = Quantity<Dimension<0, 0, 1>>;    // секунды
+	using VelocityQ = Quantity<Dimension<1, 0, -1>>;   // метры в секунду
+	using AccelQ = Quantity<Dimension<1, 0, -2>>;   // ускорение, метры в секунду в квадрате
+	using ForceQ = Quantity<Dimension<1, 1, -2>>;   // сила в ньютонах
+	
 	LengthQ   l{ 30000 };      // 30 км
 	TimeQ     t{ 10 * 60 };    // 10 минут
-							   // вычисление скорости
-	//VelocityQ v = l / t;     // результат типа VelocityQ, 50 м/с
+	t = 2 * t;						   // вычисление скорости
+	VelocityQ v = l / t;     // результат типа VelocityQ, 50 м/с
 
-	//AccelQ    a{ 9.8 };        // ускорение свободного падения
-	//MassQ     m{ 80 };         // 80 кг
-							   // сила притяжения, которая действует на тело массой 80 кг
-	//ForceQ    f = m * a;     // результат типа ForceQ
+    AccelQ    a{ 9.8 };        // ускорение свободного падения
+	MassQ     m{ 80 };         // 80 кг
+	// сила притяжения, которая действует на тело массой 80 кг
+	ForceQ    f = m * a;     // результат типа ForceQ
+	//NumberQ   n = f / (m * a);
+	std::cout << t.value() << '\n';
 	return 0;
 }
